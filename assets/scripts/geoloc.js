778971c8ee038740626db67e2ce8fd19c0f2aa18
Larissa Araújo <142195233+larissaaraujohot@users.noreply.github.com>
2023-08-22 18:51:34 -0300

    Add files via upload

---
+++ assets/scripts/geoloc.js
@@ -0,0 +1,19 @@
+function getGeolocationByIP() {
+    const url = 'https://ipapi.co/json/';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const currentCity = data.city;
+            const cityElements = document.querySelectorAll("model-city");
+            
+            cityElements.forEach(element => {
+                element.textContent = currentCity;
+            });
+        })
+        .catch(error => {
+            console.error("Error to get city name:", error);
+        });
+}
+
+getGeolocationByIP();
